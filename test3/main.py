ce77a587119375e42ef8c331bbdbe790e324d337
Andrew Hong <35881688+NovialRiptide@users.noreply.github.com>
2021-04-25 13:27:35 -0400

    trh

--- test3/main.py
+++ test3/main.py
@@ -1,20 +1,27 @@
 from ai import *
 import pygame
+import sys
+
+SCREEN_SIZE = point(750, 750)
+START = point(SCREEN_SIZE.x/2, SCREEN_SIZE.y/2)
+END = point(SCREEN_SIZE.x/2, SCREEN_SIZE.y-50)
 
 pygame.init()
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((SCREEN_SIZE.x, SCREEN_SIZE.y))
 
-d = dot()
-x = 0
-y = 0
-for _dir in d.directions:
-    x += _dir.x
-    y += _dir.y
-    print(x, y)
-    
-d.mutate()
-x = 0
-y = 0
-for _dir in d.directions:
-    x += _dir.x
-    y += _dir.y
-    print(x, y)
+p = population(100, START, END, SCREEN_SIZE)
+screen.fill((0,0,0))
+while(True):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    screen.fill((0,0,0))
+    p.draw(screen)
+    pygame.draw.circle(screen, (255,255,255), (END.x, END.y), 5)
+    p.new_generation()
+    #print("d")
+
+    clock.tick(1)
+    pygame.display.update()
