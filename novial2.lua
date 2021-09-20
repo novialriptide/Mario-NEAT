b352c61ac75201103194d98a4e472b92e05a41bf
Andrew Hong <35881688+NovialRiptide@users.noreply.github.com>
2021-09-20 01:15:56 -0400

    Update novial2.lua

--- novial2.lua
+++ novial2.lua
@@ -738,8 +738,8 @@ new_inital_generation(config.pop_size)
 focus_generation = generations[focus_generation_key]
 focus_generation:mutate_genomes()
 
-focus_generation.species[1].genomes[1].connections = {}
-focus_generation.species[1].genomes[1]:add_connection(13*17, 13*17+3)
+-- focus_generation.species[1].genomes[1].connections = {}
+-- focus_generation.species[1].genomes[1]:add_connection(13*17, 13*17+3)
 
 focus_species = focus_generation.species[focus_species_key]
 focus_genome = focus_species.genomes[focus_genome_key]
